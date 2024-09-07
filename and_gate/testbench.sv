c8cbf60b0b961dc9b8a165f2084d7ceb7676497b
Sandeep Panigrahi <156462298+Blessedsan123@users.noreply.github.com>
2024-09-07 19:23:04 +0530

    Add files via upload

---
+++ and_gate/testbench.sv
@@ -0,0 +1,28 @@
+module test_And_Gate;
+  
+  reg in1,in2;
+  wire out;
+  
+  And_Gate uut(
+    .in1(in1),
+    .in2(in2),
+    .out(out));
+  
+  initial
+    begin
+      
+      in1 = 0;
+      in2 = 1;
+      #100;
+      
+      in1 = 1;
+      in2 =1;
+      #100;
+      
+      $display("output is : %b",out);
+    end
+  
+  
+  
+endmodule
+      
